7b883656582ce8f4d2d72f13ce150fb15ad7eb54
rassoll <rassoll32@gmail.com>
2020-03-12 22:14:09 +0300

    Section 5 - Step 01

---
+++ src/main/java/com/github/rakhmedovrs/springboot/deepdive/Application.java
@@ -0,0 +1,18 @@
+package com.github.rakhmedovrs.springboot.deepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author RakhmedovRS
+ * @created 12-Mar-20
+ */
+@SpringBootApplication
+public class Application
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+	}
+}
