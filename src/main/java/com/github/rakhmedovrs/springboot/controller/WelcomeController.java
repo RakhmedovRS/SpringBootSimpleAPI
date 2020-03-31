b986fc582710dcdbcaec0ff35edd26ba8499ff23
rassoll <rassoll32@gmail.com>
2020-03-31 22:42:47 +0300

    Section 5 - Step 17

--- src/main/java/com/github/rakhmedovrs/springboot/controller/WelcomeController.java
+++ src/main/java/com/github/rakhmedovrs/springboot/controller/WelcomeController.java
@@ -1,9 +1,13 @@
 package com.github.rakhmedovrs.springboot.controller;
 
+import com.github.rakhmedovrs.springboot.configuration.BasicConfiguration;
 import com.github.rakhmedovrs.springboot.service.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author RakhmedovRS
@@ -14,10 +18,22 @@ public class WelcomeController
 {
 	@Autowired
 	private WelcomeService welcomeService;
+	@Autowired
+	private BasicConfiguration basicConfiguration;
 
 	@RequestMapping(path = "/welcome")
 	public String welcome()
 	{
 		return welcomeService.getWelcomeResponse();
+	}
+
+	@RequestMapping(path = "/dynamic-configuration")
+	public Map<String, Object> dynamicConfiguration()
+	{
+		Map<String, Object> map = new HashMap<>();
+		map.put("value", basicConfiguration.isValue());
+		map.put("number", basicConfiguration.getNumber());
+		map.put("message", basicConfiguration.getMessage());
+		return map;
 	}
 }
