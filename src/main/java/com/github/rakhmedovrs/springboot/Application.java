8773edcd58f1a12018c3fd9e1a67fa76c4241351
rassoll <rassoll32@gmail.com>
2020-03-31 22:26:39 +0300

    Section 5 - Step 16

--- src/main/java/com/github/rakhmedovrs/springboot/Application.java
+++ src/main/java/com/github/rakhmedovrs/springboot/Application.java
@@ -3,6 +3,8 @@ package com.github.rakhmedovrs.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author RakhmedovRS
@@ -14,5 +16,12 @@ public class Application
 	public static void main(String[] args)
 	{
 		ApplicationContext context = SpringApplication.run(Application.class, args);
+	}
+
+	@Profile("dev")
+	@Bean
+	public String dummyBean()
+	{
+		return "DUMMY";
 	}
 }
