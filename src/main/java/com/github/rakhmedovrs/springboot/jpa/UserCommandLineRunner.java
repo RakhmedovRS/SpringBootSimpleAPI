083e0472df1ba0af7e2ea09b3c8f1751b655af9e
rassoll <rassoll32@gmail.com>
2020-04-01 22:55:16 +0300

    Section 5 - Step 19

--- src/main/java/com/github/rakhmedovrs/springboot/jpa/UserCommandLineRunner.java
+++ src/main/java/com/github/rakhmedovrs/springboot/jpa/UserCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.github.rakhmedovrs.springboot.jpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +13,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserCommandLineRunner implements CommandLineRunner
 {
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserCommandLineRunner.class);
+
+	@Autowired
+	private UserRepository repository;
+
 	@Override
 	public void run(String... args) throws Exception
 	{
-		System.out.println("UserCommandLineRunner");
+		repository.save(new User("user", "user"));
+		repository.save(new User("administrator", "administrator"));
+		repository.save(new User("manager", "manager"));
+
+		for (User user: repository.findAll())
+		{
+			LOGGER.info(user.toString());
+		}
 	}
 }
