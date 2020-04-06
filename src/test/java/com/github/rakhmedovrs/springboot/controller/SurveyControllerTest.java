c95c8f57c7ba62a3e5796d8a34475683ff48a81a
rassoll <rassoll32@gmail.com>
2020-04-06 22:45:23 +0300

    Section 5 - Step 24

---
+++ src/test/java/com/github/rakhmedovrs/springboot/controller/SurveyControllerTest.java
@@ -0,0 +1,63 @@
+package com.github.rakhmedovrs.springboot.controller;
+
+import com.github.rakhmedovrs.springboot.model.Question;
+import com.github.rakhmedovrs.springboot.service.SurveyService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+
+/**
+ * @author RakhmedovRS
+ * @created 02-Apr-20
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(SurveyController.class)
+public class SurveyControllerTest
+{
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockBean
+	private SurveyService surveyService;
+
+	@Test
+	public void test() throws Exception
+	{
+		Question mockQuestion = new Question("Question1",
+			"Largest Country in the World", "Russia", Arrays.asList(
+			"India", "Russia", "United States", "China"));
+
+		String expected = "{\n" +
+			"    \"id\": \"Question1\",\n" +
+			"    \"description\": \"Largest Country in the World\",\n" +
+			"    \"correctAnswer\": \"Russia\",\n" +
+			"    \"options\": [\n" +
+			"        \"India\",\n" +
+			"        \"Russia\",\n" +
+			"        \"United States\",\n" +
+			"        \"China\"\n" +
+			"    ]\n" +
+			"}";
+
+		Mockito.when(surveyService.retrieveQuestion(Mockito.anyString(), Mockito.anyString())).thenReturn(mockQuestion);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+			.get("/survey/Survey1/questions/Question1")
+			.accept(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+	}
+}
